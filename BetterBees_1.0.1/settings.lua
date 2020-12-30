cbfee3f2fcb56ef09182621342168b4fb6b03df2
JacobD <jacobdicken2005@gmail.com>
2020-12-30 09:49:40 +0000

    added no biters setting

--- BetterBees_1.0.1/settings.lua
+++ BetterBees_1.0.1/settings.lua
@@ -3,18 +3,21 @@ data:extend({
         type = 'bool-setting',
         name = 'better-bees-no-resources-setting',
         setting_type = 'startup',
-        default_value = false
+        default_value = false,
+        order = 'a'
     },
     {
         type = 'bool-setting',
         name = 'better-bees-recipe-override-setting',
         setting_type = 'startup',
-        default_value = true
+        default_value = true,
+        order = 'b'
     },
     {
         type = 'bool-setting',
         name = 'better-bees-no-biters-setting',
         setting_type = 'startup',
-        default_value = true
+        default_value = true,
+        order = 'c'
     }
 })
